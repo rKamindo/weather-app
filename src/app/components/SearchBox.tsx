cebc7306f661b782224dc45672898a21fed10222
rKAmindo <randykamindo@gmail.com>
2024-02-14 12:42:19 -0500

    Add props and add cn function

--- src/app/components/SearchBox.tsx
+++ src/app/components/SearchBox.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { IoSearch } from 'react-icons/io5'
 
-type Props = {}
+type Props = {
+  className?: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+}
 
-export default function SearchBox({}: Props) {
+export default function SearchBox(props: Props) {
   return (
-    <form className="flex relative items-center justify-center h-10">
+    <form 
+      onSubmit={props.onSubmit}
+      className="flex relative items-center justify-center h-10"
+      
+    >
         <input 
             type="text" 
+            value={props.value}
+            onChange={props.onChange}
             placeholder="Search location..."
             className="px-4 py-2 w-[230px] border
             border-gray-300 rounded-l-md focus:outline-none
